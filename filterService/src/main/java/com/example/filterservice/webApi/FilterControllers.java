e3488a79c3c4b69b2743155871a76bda1e7440a1
BERNA ÖZGEN <brnozgen@hotmail.com>
2023-01-10 01:57:21 +0300

    filter service updated

--- filterService/src/main/java/com/example/filterservice/webApi/FilterControllers.java
+++ filterService/src/main/java/com/example/filterservice/webApi/FilterControllers.java
@@ -1,12 +1,107 @@
 package com.example.filterservice.webApi;
 
-import lombok.AllArgsConstructor;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.common.utilities.result.DataResult;
+import com.example.common.utilities.result.Result;
+import com.example.filterservice.business.abstracts.FilterService;
+import com.example.filterservice.business.requests.create.CreateBrandRequest;
+import com.example.filterservice.business.requests.create.CreateMainCategoryRequest;
+import com.example.filterservice.business.requests.create.CreateProductRequest;
+import com.example.filterservice.business.requests.create.CreateSecondCategoryRequest;
+import com.example.filterservice.business.requests.update.UpdateBrandRequest;
+import com.example.filterservice.business.requests.update.UpdateMainCategoryRequest;
+import com.example.filterservice.business.requests.update.UpdateProductRequest;
+import com.example.filterservice.business.requests.update.UpdateSecondCategoryRequest;
+import com.example.filterservice.business.responses.getAll.GetAllBrandResponse;
+import com.example.filterservice.business.responses.getAll.GetAllFilterResponse;
+import com.example.filterservice.business.responses.getAll.GetAllMainCategoryResponse;
+import com.example.filterservice.business.responses.getAll.GetAllProductResponse;
+import com.example.filterservice.business.responses.getAll.GetAllSecondCategoryResponse;
+
+import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/filter")
 public class FilterControllers {
+	private FilterService filterService;
 
+	// Create Operations
+	Result addBrand(CreateBrandRequest createBrandRequest) {
+		return filterService.addBrand(createBrandRequest);
+	}
+
+	Result addProduct(CreateProductRequest createProductRequest) {
+		return filterService.addProduct(createProductRequest);
+	}
+
+	Result addMainCategory(CreateMainCategoryRequest createMainCategoryRequest) {
+		return filterService.addMainCategory(createMainCategoryRequest);
+	}
+
+	Result addSecondCategory(CreateSecondCategoryRequest createSecondCategoryRequest) {
+		return filterService.addSecondCategory(createSecondCategoryRequest);
+	}
+	// Update Operations
+
+	Result updateBrand(UpdateBrandRequest updateBrandRequest) {
+		return filterService.updateBrand(updateBrandRequest);
+	}
+
+	Result updateProduct(UpdateProductRequest updateProductRequest) {
+		return filterService.updateProduct(updateProductRequest);
+	}
+
+	Result updateMainCategory(UpdateMainCategoryRequest updateMainCategoryRequest) {
+		return filterService.updateMainCategory(updateMainCategoryRequest);
+	}
+
+	Result updateSecondCategory(UpdateSecondCategoryRequest updateSecondCategoryRequest) {
+		return filterService.updateSecondCategory(updateSecondCategoryRequest);
+	}
+
+	// Read Product
+	DataResult<List<GetAllProductResponse>> getAllProduct() {
+		return filterService.getAllProduct();
+	}
+
+	DataResult<List<GetAllFilterResponse>> getByProductName(String productName) {
+		return filterService.getByProductName(productName);
+	}
+
+	DataResult<List<GetAllFilterResponse>> getByProductColor(String productColor) {
+		return filterService.getByProductColor(productColor);
+	}
+
+	DataResult<List<GetAllFilterResponse>> getByProductUnitPrice(double productUnitPrice) {
+		return filterService.getByProductUnitPrice(productUnitPrice);
+	}
+
+	DataResult<List<GetAllFilterResponse>> getByProductRank(double productRank) {
+		return filterService.getByProductRank(productRank);
+	}
+
+	// Read Brand
+
+	DataResult<List<GetAllBrandResponse>> getAllBrand() {
+		return filterService.getAllBrand();
+	}
+
+	DataResult<List<GetAllFilterResponse>> getByBrandName(String brandName) {
+		return filterService.getByBrandName(brandName);
+	}
+
+	// Read Main-Category
+	DataResult<List<GetAllMainCategoryResponse>> getAllMainCategoryName() {
+		return filterService.getAllMainCategoryName();
+	}
+
+	// Read Second-Category
+	DataResult<List<GetAllSecondCategoryResponse>> getAllSecondCategoryName() {
+		return filterService.getAllSecondCategoryName();
+	}
 }
